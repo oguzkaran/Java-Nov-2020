5cd2492b7f1129b024b8d9bf9a38a49fb72139bd
Oğuz Karan <oguzkaran@csystem.org>
2020-12-20 16:54:47 +0300

    Java-Nov-2020

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,18 +1,46 @@
 /*--------------------------------------------------------------------------------------------------------------------
- 	Atama operatörü sağdan sola (right associative) önceliklidir
+	  Aşağıdaki örnekte kod tekrarı yapılmadığına yani iş yapan başka bir metodun çağrıldığına dikkat ediniz.
+	  Yazılım geliştirmede kod tekrarı yapmamaya "DO NOT REPEAT YOURSELF" cümlesi kullanılır. Zorunlu olmadıkça
+	  programı kod tekrarı yapmamalıdır
 ---------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
-	public static void main(String [] args) 
+	public static void main(String [] args)
 	{	
-		int a, b, c;
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		System.out.print("Bir sayı giriniz:");
+		int val = Integer.parseInt(kb.nextLine());
 		
-		a = b = c = 10;
-		
-		System.out.printf("a=%d%n", a);
-		System.out.printf("b=%d%n", b);
-		System.out.printf("c=%d%n", c);
+		Util.displayEvenStatusByFlag(NumberUtil.isEven(val));		
+		Util.displayOddStatusByFlag(NumberUtil.isOdd(val));
+	}
+}
+
+class NumberUtil {
+	public static boolean isEven(int val)
+	{
+		return val % 2 == 0;
+	}
+	
+	public static boolean isOdd(int val)
+	{
+		return !isEven(val);
+	}
+}
+
+class Util {
+	public static void displayEvenStatusByFlag(boolean flag)
+	{
+		if (flag)
+			System.out.println("Çift sayı girdiniz");
+		else
+			System.out.println("Tek sayı girdiniz");
+	}
+	
+	public static void displayOddStatusByFlag(boolean flag)
+	{
+		displayEvenStatusByFlag(!flag);
 	}
 }
 
