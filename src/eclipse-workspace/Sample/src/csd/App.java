b157c75fc47e521579bdafbb90045e9172c8fc31
Oğuz Karan <oguzkaran@csystem.org>
2020-12-26 15:55:38 +0300

    Java-Nov-2020

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,28 +1,52 @@
 /*--------------------------------------------------------------------------------------------------------------------
-	Kontrolün başta yapıldığı while döngü deyiminin genel biçimi:
-	
-	while (<koşul ifadesi>)
-		<deyim>
-		
-	Koşul ifadesi boolean türden olmalıdır. Bu döngü koşul ifadesinin true olduğu sürece yinelenir 
+	Sınıf Çalışması: Parametresi ile aldığı int türden bir sayının basamak toplamını döndüren sumDigits metodunu
+	NumberUtil isimli bir sınıf içerisinde yazınız ve klavyeden girilen değerlerle çağırarak test ediniz. Metot 
+	negatif için de basamakları toplamını pozitif olarak döndürecektir	 
 ---------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{	
+		CountDigitsTest.run();				
+	}
+}
+
+
+class CountDigitsTest {
+	public static void run()
+	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
+		
 		System.out.print("Bir sayı giriniz:");
-		int n = Integer.parseInt(kb.nextLine());
+		int val;
 		
-		int i = 0;
+		while ((val = Integer.parseInt(kb.nextLine())) != 0) {
+			int count = NumberUtil.countDigits(val);
+			
+			System.out.printf("%d sayısının basamak sayısı:%d%n", val, count);
+			System.out.print("Bir sayı giriniz:");
+		}
 		
-		while (i < n) {
-			System.out.printf("%d ", i);
-			++i;
-		}	
-		System.out.printf("%nDöngü sonrası:i = %d%n", i);
-		System.out.println("Tekrar yapıyor musunuz?");
+		System.out.printf("0 sayısının basamak sayısı:%d%n", NumberUtil.countDigits(0));
+		System.out.println("Tekrar yapıyor musunuz?");		
+	}
+}
+
+class NumberUtil {
+	public static int countDigits(int val)
+	{
+		if (val == 0)
+			return 1;
+		
+		int count = 0;
+		
+		while (val != 0) {
+			++count;
+			val /= 10;			
+		}
+		
+		return count;
 	}
 }
 
