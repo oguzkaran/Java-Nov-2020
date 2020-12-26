d57cfb5a112eb40f72ea56efd00a338afff0912c
Oğuz Karan <oguzkaran@csystem.org>
2020-12-26 16:53:17 +0300

    Java-Nov-2020

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,49 +1,23 @@
 /*--------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Parametresi ile aldığı int türden bir sayının tersini döndüren reversed isimli metodu
-	yazınız ve test ediniz.
-	
-	123 -> 3 -> 
+	Yukarıdaki örnek aşağıdaki gibi yapıalbilir  	 
 ---------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{	
-		SumDigitsTest.run();				
-	}
-}
-
-class SumDigitsTest {
-	public static void run()
-	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
-		System.out.print("Bir sayı giriniz:");
 		int val;
-		
-		while ((val = Integer.parseInt(kb.nextLine())) != 0) {
-			int sum = NumberUtil.sumDigits(val);
-			
-			System.out.printf("%d sayısının basamakları toplamı:%d%n", val, sum);
+		do {
 			System.out.print("Bir sayı giriniz:");
-		}
+			val = Integer.parseInt(kb.nextLine());
+			
+			System.out.printf("%d * %d = %d\n", val, val, val * val);
+		} while (val != 0);
 		
-		System.out.printf("0 sayısının basamakları toplamı:%d%n", NumberUtil.sumDigits(0));
-		System.out.println("Tekrar yapıyor musunuz?");		
+		System.out.println();
 	}
 }
 
-class NumberUtil {
-	public static int sumDigits(int val)
-	{		
-		int sum = 0;		
-		
-		while (val > 0) {
-			sum += val % 10;
-			val /= 10;			
-		}
-		
-		return Math.abs(sum);
-	}
-}
 
