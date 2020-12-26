b781e8fcaccb679893eeb086d8f0fa5b74515067
Oğuz Karan <oguzkaran@csystem.org>
2020-12-26 16:16:48 +0300

    Java-Nov-2020

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,19 +1,19 @@
 /*--------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Parametresi ile aldığı int türden bir sayının basamak toplamını döndüren sumDigits metodunu
-	NumberUtil isimli bir sınıf içerisinde yazınız ve klavyeden girilen değerlerle çağırarak test ediniz. Metot 
-	negatif için de basamakları toplamını pozitif olarak döndürecektir	 
+	Sınıf Çalışması: Parametresi ile aldığı int türden bir sayının tersini döndüren reversed isimli metodu
+	yazınız ve test ediniz.
+	
+	123 -> 3 -> 
 ---------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{	
-		CountDigitsTest.run();				
+		SumDigitsTest.run();				
 	}
 }
 
-
-class CountDigitsTest {
+class SumDigitsTest {
 	public static void run()
 	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
@@ -22,31 +22,28 @@ class CountDigitsTest {
 		int val;
 		
 		while ((val = Integer.parseInt(kb.nextLine())) != 0) {
-			int count = NumberUtil.countDigits(val);
+			int sum = NumberUtil.sumDigits(val);
 			
-			System.out.printf("%d sayısının basamak sayısı:%d%n", val, count);
+			System.out.printf("%d sayısının basamakları toplamı:%d%n", val, sum);
 			System.out.print("Bir sayı giriniz:");
 		}
 		
-		System.out.printf("0 sayısının basamak sayısı:%d%n", NumberUtil.countDigits(0));
+		System.out.printf("0 sayısının basamakları toplamı:%d%n", NumberUtil.sumDigits(0));
 		System.out.println("Tekrar yapıyor musunuz?");		
 	}
 }
 
 class NumberUtil {
-	public static int countDigits(int val)
-	{
-		if (val == 0)
-			return 1;
+	public static int sumDigits(int val)
+	{		
+		int sum = 0;		
 		
-		int count = 0;
-		
-		while (val != 0) {
-			++count;
+		while (val > 0) {
+			sum += val % 10;
 			val /= 10;			
 		}
 		
-		return count;
+		return Math.abs(sum);
 	}
 }
 
