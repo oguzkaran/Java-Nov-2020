6331a868eaa5070d682b84a0b0e39b0143ea3ac2
Oğuz Karan <oguzkaran@csystem.org>
2021-01-02 13:49:54 +0300

    Java-Nov-2020

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,86 +1,39 @@
 /*--------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Parametresi ile aldığı int türden bir n değeri için n-inci asal sayıyı döndüren getPrime
-	isimli metodu yazınız ve test ediniz:
-	Açıklamalar:
-	- isPrime metosunu kullanınız
-	- isPrime metodunun hızlı olan versiyonunu kullandığınız bilinciyle yazınız
-	- Metot n-nin pozitif olmayan değerleri için -1 sayını döndürecektir
-	- Metodu klavyeden alınan değerlerle çağırarak test ediniz 	
+	Sınıf Çalışması: Parametresi ile aldığı int türden sayıdan büyük ilk Fibonacci sayısını döndüren getNextFibonacciNumber
+	metodunu yazınız ve test ediniz
 ---------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{
-		GetPrimeTest.run();
+		GetNextFibonacciTest.run();
 	}
 }
 
-class GetPrimeTest {
+class GetNextFibonacciTest {
 	public static void run()
 	{		
 		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
 		for (;;) {
 			System.out.print("Bir sayı giriniz:");
-			int n = Integer.parseInt(kb.nextLine());
+			int val = Integer.parseInt(kb.nextLine());
 			
-			if (n <= 0)
+			if (val <= -999)
 				return;
 			
-			System.out.printf("%d. asal sayı:%d%n", n, NumberUtil.getPrime(n));
+			System.out.printf("%d sayısından sonraki ilk Fibonnacci sayısı:%d%n", val, NumberUtil.getNextFibonacciNumber(val));
 		}
 		
 	}
 }
 
 class NumberUtil {
-	public static int getPrime(int n)
+	public static int getNextFibonacciNumber(int val)
 	{
-		if (n <= 0)
-			return -1;
 		
-		int count = 0;
-		int val = 2;		
-		
-		for (;;) {
-			if (isPrime(val))
-				++count;
-			
-			if (count == n)
-				return val;
-			
-			++val;
-		}		
-	}
-	
-	public static boolean isPrime(int val)
-	{
-		if (val <= 1)
-			return false;
-		
-		if (val % 2 == 0)
-			return val == 2;
-		
-		if (val % 3 == 0)
-			return val == 3;
-		
-		if (val % 5 == 0)
-			return val == 5;
-		
-		if (val % 7 == 0)
-			return val == 7;
-	
-		
-		for (int i = 11; i * i <= val; i += 2)			
-			if (val % i == 0)
-				return false;		
-		
-		return true;
-		
-	}
+	}	
 }
-
-
 
 
