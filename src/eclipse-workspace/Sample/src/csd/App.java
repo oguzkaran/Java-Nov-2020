c649701506bc3f099d75821190b32c37fb4dda50
Oğuz Karan <oguzkaran@csystem.org>
2021-01-16 14:37:57 +0300

    Java-Nov-2021

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,67 +1,25 @@
 /*--------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Parametresi ile aldığı bir sayının Armstrong sayısı olup olmadığını test eden isArmstrong
-	metodunu NumberUtil sınıfı içerisinde yazınız ve test ediniz. 
-	
-	Açıklamalar:
-	- Kuvvet alma işlemi için Math sınıfının pow metodunu kullanmayınız
-	
-	- Bir sayının her basamağının basamak sayıncı kuvvetleri alınıp toplandığında sonuç sayının kendisine eşitse, bu 
-	sayı bir Armstrong sayısıdır
-	Örneğin: 153 -> 1 * 1 * 1 + 5 * 5 * 5 + 3 * 3 * 3 = 153
-	
-	- Sayı negatif ise false döndürebilirsiniz
-	
-	- Test işlemini 1, 2, 3, 4, 5, 6 ve 7 basamaklı Armstrong sayılarını ekrana yazdırarak yapabilirsiniz
-	
-	Örnekte Math.pow metodunun += operatörü ile kullanımına ve countDigits metoduna dikkat ediniz
+	Yukarıdaki örnek parantez konarak daha okunabilir hale getirilebilir	
 ---------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{
-		IsArmstrongTest.run();
-	}
-}
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		System.out.print("Birinci sayıyı giriniz:");
+		int a = Integer.parseInt(kb.nextLine());
+		
+		System.out.print("İkinci sayıyı giriniz:");
+		int b = Integer.parseInt(kb.nextLine());	
+		
+		System.out.print("Üçüncü sayıyı giriniz:");
+		int c = Integer.parseInt(kb.nextLine());
 
-
-class IsArmstrongTest {
-	public static void run()
-	{
-		for (int n = -100; n <= 9999999; ++n)
-			if (NumberUtil.isArmstrong(n))
-				System.out.println(n);
+		int min;
+		
+		min = (a < b) ? (a < c ? a : c) : (b < c ? b : c); 
+		
+		System.out.printf("min(%d, %d, %d) = %d%n", a, b, c, min);		
 	}
-}
-
-class NumberUtil {
-	public static boolean isArmstrong(int val)
-	{
-		if (val < 0)
-			return false;
-		
-		return getDigitstPowSum(val) == val;	
-	}
-	
-	public static int getDigitstPowSum(int val)
-	{
-		int n = countDigits(val);		
-		int sum = 0;
-		
-		while (val != 0) {
-			sum += Math.pow(val % 10, n);
-			val /= 10;
-		}
-		
-		return sum;				
-	}
-	
-	public static int countDigits(long val)
-	{
-		if (val == 0)
-			return 1;
-		
-		return (int)Math.log10(Math.abs(val)) + 1;
-	}	
-	
 }
