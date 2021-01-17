4c0cf4ddc5f486d8d135578136b2de49d6a2a5a8
Oğuz Karan <oguzkaran@csystem.org>
2021-01-17 14:45:43 +0300

    Java-Nov-2020

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,47 +1,34 @@
 /*--------------------------------------------------------------------------------------------------------------------	
-	
+	Yerel değişkenler ve parametre değişkenleri stack alanında yaratılır. Yerel değişkenin ömrü akış bakımından 
+	bildirildiği yerde yaratılır, bildirildiği bloğun sonunda yok edilir. Parametre değişkenleri akış bakımından 
+	metot çağrısında yaratılır, metot çağrısı bittiğinde yok edilir
 ---------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{
-		short a = 10;
-		float b = 30;
+		{
+			//...
+			int a; //Akış buradayken stack'de yaratıldı
+			int b; //Akış buradayken stack'de yaratıldı
+			
+			//...
+		} // a ve b yok edildi
 		
-		Sample.foo(a, b);
+		
+		Sample.foo(20, 56); //Parametre değişkenleri yaratıldı ve metot çağrısı bittiğinde yok edildi
+		
+		//...
+		
+		Sample.foo(20, 56); //Parametre değişkenleri yaratıldı ve metot çağrısı bittiğinde yok edildi
 	}
 }
 
 class Sample {
-	public static void foo() // #1
+	public static void foo(int a, int b)
 	{
-		System.out.println("Sample, foo");	
-	}
-	
-	public static void foo(int a) //#2
-	{
-		System.out.println("Sample, foo, int");
-	}		
-	
-	public static void foo(long a, int b) //#3
-	{
-		System.out.println("Sample, foo, double, int");
-	}
-	
-	public static void foo(int a, long b) //#4
-	{
-		System.out.println("Sample, foo, int, long");
-	}
-	
-	public static void foo(int a, float b) //#5
-	{
-		System.out.println("Sample, foo, int, float");
-	}
-	
-	public static void bar(int a) //#6
-	{
-		System.out.println("Sample, bar, int");
+		//...
 	}
 }
 
