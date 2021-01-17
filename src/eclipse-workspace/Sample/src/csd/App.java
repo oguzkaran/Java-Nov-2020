b79c0eeb804219f723b07b1a259151ca5c2e852e
Oğuz Karan <oguzkaran@csystem.org>
2021-01-17 16:52:39 +0300

    Java-Nov-2020

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,34 +1,20 @@
 /*--------------------------------------------------------------------------------------------------------------------	
-	Yerel değişkenler ve parametre değişkenleri stack alanında yaratılır. Yerel değişkenin ömrü akış bakımından 
-	bildirildiği yerde yaratılır, bildirildiği bloğun sonunda yok edilir. Parametre değişkenleri akış bakımından 
-	metot çağrısında yaratılır, metot çağrısı bittiğinde yok edilir
+	Aşağıdaki örnekte s yerel değişkenine değer atanmadan kullanıldığı error oluşur
 ---------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
-	{
-		{
-			//...
-			int a; //Akış buradayken stack'de yaratıldı
-			int b; //Akış buradayken stack'de yaratıldı
-			
-			//...
-		} // a ve b yok edildi
+	{		
+		Sample s;
 		
-		
-		Sample.foo(20, 56); //Parametre değişkenleri yaratıldı ve metot çağrısı bittiğinde yok edildi
-		
-		//...
-		
-		Sample.foo(20, 56); //Parametre değişkenleri yaratıldı ve metot çağrısı bittiğinde yok edildi
+		s.x = 10; //error				
 	}
 }
 
 class Sample {
-	public static void foo(int a, int b)
-	{
-		//...
-	}
+	public int x;
+	public boolean y;
+	//...
 }
 
