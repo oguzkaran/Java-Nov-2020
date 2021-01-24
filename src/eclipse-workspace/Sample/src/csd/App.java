73f7d01bbc55f66ae6f906eb3d757e2058b6788e
Oğuz Karan <oguzkaran@csystem.org>
2021-01-24 16:47:18 +0300

    Java-Nov-2020

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,37 +1,53 @@
 /*--------------------------------------------------------------------------------------------------------------------		
-	 Point sınıfı ve test kodu
+	Sınıf Çalışması: İki zar atıldığında zarların çift gelmesi (ikisi de aynı) olasılığını yaklaşık olarak hesaplayan
+	basit bir simülasyon programını yazınız
 ---------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{
-		Point p = new Point();
-		
-		p.x = 45;
-		p.y = -67;
-		
-		p.display();		
-		
-		p.offset(-20, 24);
-		
-		p.display();
+		DiceSimulationApp.run();	
 	}
 }
 
-class Point {
-	public int x;
-	public int y;
-	
-	
-	public void offset(int dx, int dy)
+class DiceSimulationApp {
+	public static void run()
 	{
-		x += dx;
-		y += dy;
-	}
-	
-	public void display()
-	{
-		System.out.printf("{x : %d, y : %d}%n", x, y);
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		
+		System.out.print("Kaç kez atmak istiyorsunuz?");
+		int n = Integer.parseInt(kb.nextLine());
+		
+		DiceSimulation diceSimulation = new DiceSimulation();
+		
+		diceSimulation.run(n);
+		
+		System.out.printf("p = %.10f%n", diceSimulation.p);		
 	}
 }
+
+class DiceSimulation {
+	public double p;
+	
+	//...
+	
+	public static boolean areSame(java.util.Random r)
+	{
+		return r.nextInt(6) == r.nextInt(6); 
+	}
+	
+	public void run(int n)
+	{
+		java.util.Random random = new java.util.Random();
+		
+		int count = 0;
+		
+		for (int i = 0; i < n; ++i)
+			if (areSame(random))
+				++count;
+		
+		p = (double) count/ n;
+	}
+}
+
