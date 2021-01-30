f30aad103632c20f58f1efe03df08f21c82ad904
Oğuz Karan <oguzkaran@csystem.org>
2021-01-30 14:36:16 +0300

    Java-Nov-2020

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,53 +1,42 @@
 /*--------------------------------------------------------------------------------------------------------------------		
-	Sınıf Çalışması: İki zar atıldığında zarların çift gelmesi (ikisi de aynı) olasılığını yaklaşık olarak hesaplayan
-	basit bir simülasyon programını yazınız
+	ctor çağrılmadan önce non-static veri elemanlarına default değerler atanmış olur
 ---------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
-	{
-		DiceSimulationApp.run();	
+	{		
+		Sample s1 = new Sample(10);
+		Sample s2 = new Sample();
+		Sample s3 = new Sample(34);
+		
+		//...						
 	}
 }
 
-class DiceSimulationApp {
-	public static void run()
+class Sample {
+	public int x;
+	public boolean y;
+	
+	public Sample()
 	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		
-		System.out.print("Kaç kez atmak istiyorsunuz?");
-		int n = Integer.parseInt(kb.nextLine());
-		
-		DiceSimulation diceSimulation = new DiceSimulation();
-		
-		diceSimulation.run(n);
-		
-		System.out.printf("p = %.10f%n", diceSimulation.p);		
+		System.out.println("Sample()");
+		System.out.printf("x = %d%n", x);
+		System.out.printf("b = %b%n", y);
+	}	
+	
+	public Sample(double a)
+	{
+		System.out.println("Sample(double)");
+		System.out.printf("x = %d%n", x);
+		System.out.printf("b = %b%n", y);
 	}
+	
+	public Sample(int a)
+	{
+		System.out.println("Sample(int)");
+		System.out.printf("x = %d%n", x);
+		System.out.printf("b = %b%n", y);
+	}
+	
 }
-
-class DiceSimulation {
-	public double p;
-	
-	//...
-	
-	public static boolean areSame(java.util.Random r)
-	{
-		return r.nextInt(6) == r.nextInt(6); 
-	}
-	
-	public void run(int n)
-	{
-		java.util.Random random = new java.util.Random();
-		
-		int count = 0;
-		
-		for (int i = 0; i < n; ++i)
-			if (areSame(random))
-				++count;
-		
-		p = (double) count/ n;
-	}
-}
-
