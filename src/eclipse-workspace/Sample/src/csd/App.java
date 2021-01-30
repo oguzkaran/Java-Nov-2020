e3772e46936a187360c25b30c3301203aec221e1
Oğuz Karan <oguzkaran@csystem.org>
2021-01-30 18:34:50 +0300

    Java-Nov-2020

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,42 +1,94 @@
 /*--------------------------------------------------------------------------------------------------------------------		
-	ctor çağrılmadan önce non-static veri elemanlarına default değerler atanmış olur
+	Complex sınıfı ve test kodu
 ---------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{		
-		Sample s1 = new Sample(10);
-		Sample s2 = new Sample();
-		Sample s3 = new Sample(34);
+		Complex z, conjugate;
 		
-		//...						
+		z = new Complex(3, 4);
+		conjugate = z.getConjugate();
+		
+		z.display();
+		conjugate.display();		
 	}
 }
 
-class Sample {
-	public int x;
-	public boolean y;
+class Complex {
+	public static Complex add(double a1, double b1, double a2, double b2) //ileride gizlenecek
+	{		
+		return new Complex(a1 + a2, b1 + b2);
+	}
 	
-	public Sample()
+	public static Complex subtract(double a1, double b1, double a2, double b2) //ileride gizlenecek
 	{
-		System.out.println("Sample()");
-		System.out.printf("x = %d%n", x);
-		System.out.printf("b = %b%n", y);
+		return add(a1, b1, -a2, -b2);
+	}
+	
+	public double re;
+	public double im;
+	
+	public Complex()
+	{		
 	}	
 	
-	public Sample(double a)
+	public Complex(double a)
 	{
-		System.out.println("Sample(double)");
-		System.out.printf("x = %d%n", x);
-		System.out.printf("b = %b%n", y);
+		re = a;
 	}
 	
-	public Sample(int a)
+	public Complex(double a, double b)
 	{
-		System.out.println("Sample(int)");
-		System.out.printf("x = %d%n", x);
-		System.out.printf("b = %b%n", y);
+		re = a;
+		im = b;
 	}
 	
+	public double getNorm()
+	{
+		return Math.sqrt(re * re + im * im);
+	}
+	
+	public Complex getConjugate()
+	{
+		return new Complex(re, -im);		
+	}
+	
+	//add methods
+	public Complex add(Complex other)
+	{
+		return add(re, im, other.re, other.im);						
+	}
+	
+	public Complex add(double val)
+	{
+		return add(re, im, val, 0);
+	}
+	
+	public static Complex add(double val, Complex z)
+	{
+		return add(val, 0, z.re, z.im);
+	}
+	
+	//subtract methods
+	public Complex subtract(Complex other)
+	{
+		return subtract(re, im, other.re, other.im);						
+	}
+	
+	public Complex subtract(double val)
+	{
+		return subtract(re, im, val, 0);
+	}
+	
+	public static Complex subtract(double val, Complex z)
+	{
+		return subtract(val, 0, z.re, z.im);
+	}
+	
+	public void display()
+	{
+		System.out.printf("|%.2f + %.2f * i| = %f%n", re, im, getNorm());
+	}
 }
