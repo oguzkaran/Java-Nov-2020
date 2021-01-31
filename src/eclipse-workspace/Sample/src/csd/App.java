dc05fda715f55d10aa132b968e382d66adc10e93
Oğuz Karan <oguzkaran@csystem.org>
2021-01-31 14:41:39 +0300

    Java-Nov-2020

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,94 +1,17 @@
 /*--------------------------------------------------------------------------------------------------------------------		
-	Complex sınıfı ve test kodu
+	 
 ---------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{		
-		Complex z, conjugate;
+		String s = "Bugün hava çok güzel. Fakat bu çok güzel havada dışarı çıkamıyoruz";
+		String str = "güzel";
 		
-		z = new Complex(3, 4);
-		conjugate = z.getConjugate();
+		int index = s.lastIndexOf(str);
 		
-		z.display();
-		conjugate.display();		
+		System.out.printf("Index:%d%n", index);		
 	}
 }
 
-class Complex {
-	public static Complex add(double a1, double b1, double a2, double b2) //ileride gizlenecek
-	{		
-		return new Complex(a1 + a2, b1 + b2);
-	}
-	
-	public static Complex subtract(double a1, double b1, double a2, double b2) //ileride gizlenecek
-	{
-		return add(a1, b1, -a2, -b2);
-	}
-	
-	public double re;
-	public double im;
-	
-	public Complex()
-	{		
-	}	
-	
-	public Complex(double a)
-	{
-		re = a;
-	}
-	
-	public Complex(double a, double b)
-	{
-		re = a;
-		im = b;
-	}
-	
-	public double getNorm()
-	{
-		return Math.sqrt(re * re + im * im);
-	}
-	
-	public Complex getConjugate()
-	{
-		return new Complex(re, -im);		
-	}
-	
-	//add methods
-	public Complex add(Complex other)
-	{
-		return add(re, im, other.re, other.im);						
-	}
-	
-	public Complex add(double val)
-	{
-		return add(re, im, val, 0);
-	}
-	
-	public static Complex add(double val, Complex z)
-	{
-		return add(val, 0, z.re, z.im);
-	}
-	
-	//subtract methods
-	public Complex subtract(Complex other)
-	{
-		return subtract(re, im, other.re, other.im);						
-	}
-	
-	public Complex subtract(double val)
-	{
-		return subtract(re, im, val, 0);
-	}
-	
-	public static Complex subtract(double val, Complex z)
-	{
-		return subtract(val, 0, z.re, z.im);
-	}
-	
-	public void display()
-	{
-		System.out.printf("|%.2f + %.2f * i| = %f%n", re, im, getNorm());
-	}
-}
