f2f29a276a10f92dae1229b19451cfa9dc6dfe13
Oğuz Karan <oguzkaran@csystem.org>
2021-01-31 16:20:00 +0300

    Java-Nov-2020

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,39 +1,19 @@
 /*--------------------------------------------------------------------------------------------------------------------		
-	 Sınıf Çalışması: Klavyeden bir kullanıcı adı ve şifre isteyen basit bir ATM uygulamasının bir parçası olan
-	 aşağıda açıklanan programı yazınız:
-	 Açıklamalar:
-	 - Kullanıcı ve şifre 3(üç) kez denenebilecektir
-	 - Denemeler sırasında doğru giriş yapılırsa "Giriş başarılı", 3 kez denemede de yanlış olursa 
-	 "Giriş başarısız. Artık deneme hakkınız bitti" mesajı verilecektir
-	 - Kullanıcı adı ve şifrenin doğruluğu program içerisinde belirlenen bir kullanıcı adı ve şifre ile yapılacaktır
+	 Sınıf Çalışması: Parametresi ile aldığı bir yazının tersini döndüren reverse isimli metodu StringUtil sınıfı
+	 içerisinde yazınız ve metodu test ediniz
 ---------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{		
-		TrimTest.run();	
-	}
-}
-
-class TrimTest {
-	public static void run()
-	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
-		for (;;) {
-			System.out.print("Bir yazı giriniz:");
-			String s = kb.nextLine();
-			
-			if ("quit".equals(s))
-				break;			
-			
-			System.out.printf("[%s]%n", s.trim());			
-			System.out.printf("[%s]%n", s);
-		}	
+		System.out.print("Derece değerini giriniz:");
+		double degree = Double.parseDouble(kb.nextLine());	
 		
-		System.out.println("Tekrar yapıyor musunuz?");
+		
+		System.out.println("Bugün hava " + degree + " derece");
 	}
-	
 }
 
