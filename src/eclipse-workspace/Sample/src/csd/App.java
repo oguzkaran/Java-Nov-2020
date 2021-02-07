600ff2d078b7019e7f44033554681c0faa2699d2
Oğuz Karan <oguzkaran@csystem.org>
2021-02-07 14:32:34 +0300

    Java-Nov-2020

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,20 +1,46 @@
 /*--------------------------------------------------------------------------------------------------------------------		
-	 Derleyici özdeş String atomları (string literals) için aynı adresi verecek kodu üretir. Aşağıdaki örnekte 
-	 adres karşılaştırması yapılmıştır. Adresler aynı olduğu için "Aynı yazı" yazısı ekrana basılır. Anımsanacağı
-	 gibi örneğin yazılar klavyeden alınırsa farklı nesnelerin elde edildiğinden yazılar aynı bile "Farklı yazılar"
-	 yazısı ekrana basılır 
+	Sınıf Çalışması: Parametresi ile aldığı bir yazının palindrom olup olmadığını test eden isPalindrome isimli
+	metodu yazınız ve test ediniz.
+	
+	Palindrom: Yalnızca alfabetik karakterler tersten okuduğunda aynı olan yazılara denir. Yani alfabetik dışı
+	karakterler yok sayılır
+	Örnek: 
+	Ey edip Adana'da pide ye ->eyedipadanadapideye
+	Ali Papila -> alipapila
+	Anastas mum satsana -> anastasmumsatsana	
 ---------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{		
-		String s1 = "ankara";
-		String s2 = "ankara";
+		PalindromTest.run();
+	}
+}
+
+class PalindromTest {
+	public static void run()
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
-		if (s1 == s2) //Dikkat adres karşılaştırması
-			System.out.println("Aynı yazı");
-		else
-			System.out.println("Farklı yazılar");		
+		for (;;) {
+			System.out.print("Bir yazı giriniz:");
+			String s = kb.nextLine();
+			
+			if ("quit".equals(s))
+				break;
+			
+			System.out.println(StringUtil.isPalindrome(s) ? "Palindrom" : "Palindrom değil");
+		}
+		
+		System.out.println("Tekrar yapıyor musunuz?");
+		
+	}
+}
+
+class StringUtil {
+	public static boolean isPalindrome(String s)
+	{
+		
 	}
 }
