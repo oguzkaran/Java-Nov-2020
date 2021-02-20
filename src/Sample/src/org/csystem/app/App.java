84978969827cbf2239c5429eeadb880353b43730
Oğuz Karan <oguzkaran@csystem.org>
2021-02-20 15:47:42 +0300

    Java-Nov-2020

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,32 +1,21 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Bir dizi yaratılıdığında tüm elemanlarına default değerler atanır
+    Sınıf Çalışması: Parametresi ile aldığı int türden bir dizinin elemanlarının toplamını döndüren sum isimli
+    metodu ArrayUtil sınıfı içerisinde yazınız ve elemanları rasgele üretilmiş diziler ile test ediniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
-import java.util.Scanner;
+import java.util.Random;
+
+import static org.csystem.util.ArrayUtil.*;
 
 class App {
     public static void main(String [] args)
     {
-        Scanner kb = new Scanner(System.in);
-        System.out.print("Bir sayı giriniz:");
-        int n = Integer.parseInt(kb.nextLine());
-        boolean []flags;
+        Random r = new Random();
+        int [] a = new int[10];
+        fillRandomArray(r, a, 10, 20);
 
-        flags = new boolean[n];
-
-        for (int i = 0; i < flags.length; ++i)
-            System.out.printf("%b ", flags[i]);
-
-        System.out.println();
-
-        flags = new boolean[2 * n];
-
-        for (int i = 0; i < flags.length; ++i)
-            System.out.printf("%b ", flags[i]);
-
-        System.out.println();
-
+        display(a);
     }
 }
 
---
+++ src/Sample/src/org/csystem/util/ArrayUtil.java
@@ -0,0 +1,59 @@
+/*----------------------------------------------------------------------------------------------------------------------
+	ArrayUtil sınıfı
+----------------------------------------------------------------------------------------------------------------------*/
+package org.csystem.util;
+
+import java.util.Random;
+
+public class ArrayUtil {
+    public static void display(int [] a)
+    {
+        for (int i = 0; i < a.length; ++i)
+            System.out.printf("%d ", a[i]);
+
+        System.out.println();
+    }
+
+    public static void fillRandomArray(int [] a, int min, int max) //[min, max)
+    {
+        fillRandomArray(new Random(), a, min, max);
+    }
+
+    public static void fillRandomArray(Random r, int [] a, int min, int max) //[min, max)
+    {
+        for (int i = 0; i < a.length; ++i)
+            a[i] = r.nextInt(max - min) + min;
+    }
+
+    public static int [] generateRandomArray(int n, int min, int max) //[min, max)
+    {
+        return generateRandomArray(new Random(), n, min, max);
+    }
+
+    public static int [] generateRandomArray(Random r, int n, int min, int max) //[min, max)
+    {
+        int [] a = new int[n];
+
+        fillRandomArray(r, a, min, max);
+
+        return a;
+    }
+
+    public static void swap(int [] a, int i, int k)
+    {
+        int temp;
+
+        temp = a[i];
+        a[i] = a[k];
+        a[k] = temp;
+    }
+
+    public static void swap(double [] a, int i, int k)
+    {
+        double temp;
+
+        temp = a[i];
+        a[i] = a[k];
+        a[k] = temp;
+    }
+}
