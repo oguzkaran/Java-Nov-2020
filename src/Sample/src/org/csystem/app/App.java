b327078ab34eef7b0033819d4d924744e33c57f3
Oğuz Karan <oguzkaran@csystem.org>
2021-03-07 16:36:11 +0300

    Java-Nov-2020

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,44 +1,42 @@
 /*--------------------------------------------------------------------------------------------------------------------
-    Homework001-2. sorunun bir çözümü
+    Aşağıdaki Person sınıfında m_married veri elemanı için get metodu "is" ile başlatılmıştır
 ---------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
-
-import org.csystem.util.NumberUtil;
-
-import java.util.Scanner;
 
 class App {
     public static void main(String[] args)
     {
-        MidTest.run();
+
     }
 }
 
-class MidTest {
-    public static void run()
+class Person {
+    private String m_name;
+    private int m_no;
+    private boolean m_married;
+
+    //...
+
+    public String getName()
     {
-        Scanner kb = new Scanner(System.in);
+        return m_name;
+    }
 
-        for (;;) {
-            System.out.print("Birinci sayıyı giriniz:");
-            int a = Integer.parseInt(kb.nextLine());
+    public void setName(String name)
+    {
+        //...
+        m_name = name;
+    }
 
-            System.out.print("İkinci sayıyı giriniz:");
-            int b = Integer.parseInt(kb.nextLine());
+    //...
 
-            System.out.print("Üçüncü sayıyı giriniz:");
-            int c = Integer.parseInt(kb.nextLine());
+    public boolean isMarried()
+    {
+        return m_married;
+    }
 
-            System.out.println(NumberUtil.mid(a, b, c));
-            System.out.println(NumberUtil.mid(b, a, c));
-            System.out.println(NumberUtil.mid(c, a, b));
-            System.out.println(NumberUtil.mid(a, c, b));
-            System.out.println(NumberUtil.mid(c, b, a));
-            System.out.println(NumberUtil.mid(b, c, a));
-
-            if (a == 0)
-                break;
-        }
+    public void setMarried(boolean married)
+    {
+        m_married = married;
     }
 }
-
